c25cfcdefeae888498e68e9f25020563361c5e49
hamzamassir <hamza.massir-etu@etu.univh2c.ma>
2024-11-29 01:42:15 +0100

    grafana

--- src/main/java/com/hamza/microservices/apigateway/routes/Routes.java
+++ src/main/java/com/hamza/microservices/apigateway/routes/Routes.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
